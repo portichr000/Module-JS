7c091793505a3fb8e1d868daac72bbae296a1bc4
Christian <chris6por@gmail.com>
2018-06-25 14:01:47 +0000

    popular momentjs functions

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,4 +1,25 @@
 // ES6 code goes here
-import navigation, { greeting, multiply } from './navigation';
+import moment from 'moment';
 
-console.log(greeting);
+// Epoch timestamp - number of seconds that have elapsed since January 1, 1970
+moment.now();
+
+// Creating a new date object for right now
+moment();
+
+// Creating date object with a specific date/format
+const birthday = moment('1983-07-31', 'YYYY-MM-DD');
+
+// Querying a named day of the week for a date
+console.log(birthday.format('dddd'));
+
+// Date counter
+birthday.fromNow();
+
+// Custom foramtted date
+birthday.format("MMM Do YYYY");
+
+// Date calculations
+const twoWeeksFromNow = moment().add(14, 'days');
+const anotherTwoWeeksFromNow = moment().add(2, 'weeks');
+const sixMonthsAgo = moment().subtract(6, 'm');
